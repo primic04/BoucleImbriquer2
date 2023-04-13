141d0c5bfcdb8b67b15e05245861983860fde899
primic04 <mtc-dev-programming04@hotmail.com>
2023-04-13 14:48:24 -0400

    tentative maitrise boucle imbriquer

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boucleimbriquer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("debut du test");
+
+            Console.WriteLine("veux tu jouer avec moi");
+            string useranswer = Console.ReadLine();
+
+
+            if (useranswer == "oui")
+            {
+                Console.WriteLine("vous avez dit " + useranswer);
+            }
+            else if(useranswer == "non")
+            {
+                Console.WriteLine("vous avez repondu " + useranswer);
+            }
+            else
+            {
+                while (useranswer != "oui" || useranswer != "non")
+                {
+                    Console.WriteLine("veux tu jouer avec moi");
+                    useranswer = Console.ReadLine();
+                    if (useranswer == "oui")
+                    {
+                        Console.WriteLine("vous avez dit " + useranswer);
+                        break;
+                    }
+                    else if (useranswer == "non")
+                    {
+                        Console.WriteLine("vous avez repondu " + useranswer);
+                        break;
+                    }
+                    //else
+                    //{
+                        //continue;
+                    //}                    
+                }
+            }
+            Console.WriteLine("test reussis");
+         
+
+
+
+
+            Console.Read();
+        }
+    }
+}
+
